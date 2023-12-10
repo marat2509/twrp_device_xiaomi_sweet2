6cb759cbbf427a3dac4252397ef8461911d7394e
marat2509 <93652988+marat2509@users.noreply.github.com>
2023-12-11 01:06:50 +0300

    device: set TW_MAX_BRIGHTNESS

--- device.mk
+++ device.mk
@@ -61,6 +61,7 @@ TW_USE_TOOLBOX := true
 TW_INCLUDE_RESETPROP := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 1540
 TW_DEFAULT_BRIGHTNESS := 1200
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
