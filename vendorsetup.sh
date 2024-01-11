dc83b0420ca1d32894a130f2ca6bf5268e38e0f8
marat2509 <93652988+marat2509@users.noreply.github.com>
2024-01-11 18:02:56 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,74 +35,77 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if { [ -z "$1" ] || [ "$1" = "$FDEVICE" ]; } && { [ -z "$FOX_BUILD_DEVICE" ] || [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; }; then
-	# initial setup
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export FOX_BUILD_DEVICE="$FDEVICE"
- 	export LC_ALL="C"
+    # initial setup
+    export ALLOW_MISSING_DEPENDENCIES=true
+    export FOX_BUILD_DEVICE="$FDEVICE"
+    export LC_ALL="C"
 
 	# add more builtins
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_USE_NANO_EDITOR=1
- 	export FOX_USE_BASH_SHELL=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_SED_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_BASH_SHELL=1
     export OF_ENABLE_LPTOOLS=1
 
     # enable app manager
-	export FOX_ENABLE_APP_MANAGER=1
+    export FOX_ENABLE_APP_MANAGER=1
 
-	# disable led settings
-	export OF_USE_GREEN_LED=0
+    # disable led settings
+    export OF_USE_GREEN_LED=0
 
-	# maintainer info
-	export OF_MAINTAINER="marat2509"
+    # maintainer info
+    export OF_MAINTAINER="marat2509"
 
-	# screen info
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=93
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-	export OF_CLOCK_POS=1
+    # screen info
+    export OF_SCREEN_H=2400
+    export OF_STATUS_H=93
+    export OF_STATUS_INDENT_LEFT=48
+    export OF_STATUS_INDENT_RIGHT=48
+    export OF_CLOCK_POS=1
 
-	# always enabled navbar
-	export OF_ALLOW_DISABLE_NAVBAR=0
+    # always enabled navbar
+    export OF_ALLOW_DISABLE_NAVBAR=0
 
-	# prevent recovery overwriting
-	export OF_PATCH_AVB20=1
+    # prevent recovery overwriting
+    export OF_PATCH_AVB20=1
 
-	# compability fixes
-	export FOX_TARGET_DEVICES="sweet_k6a,sweet"
-	export TARGET_DEVICE_ALT=$FOX_TARGET_DEVICES
+    # compability fixes
+    export FOX_TARGET_DEVICES="sweet_k6a,sweet"
+    export TARGET_DEVICE_ALT=$FOX_TARGET_DEVICES
 
-	# Magisk
-	MAGISK_VER=$(curl -fSsl https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep tag_name | sed 's/,//g' | awk '{print $2}' | sed 's/["v]//g')
-	MAGISK_ZIP="$(pwd)/vendor/recovery/FoxFiles/Magisk-v$MAGISK_VER.zip"
+    # Magisk
+    MAGISK_VER=$(curl -fSsl https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep tag_name | sed 's/,//g' | awk '{print $2}' | sed 's/["v]//g')
+    MAGISK_ZIP="$(pwd)/vendor/recovery/FoxFiles/Magisk-v$MAGISK_VER.zip"
         if [ ! -e $MAGISK_ZIP ]; then
-	    echo "I: Downloading Magisk v$MAGISK_VER"
+            echo "I: Downloading Magisk v$MAGISK_VER"
             wget -q -O "$MAGISK_ZIP" "https://github.com/topjohnwu/Magisk/releases/download/v$MAGISK_VER/Magisk-v$MAGISK_VER.apk"
         fi;
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=$MAGISK_ZIP
+    export FOX_USE_SPECIFIC_MAGISK_ZIP=$MAGISK_ZIP
 
-	# delete unneeded addons
-	export FOX_DELETE_AROMAFM=1
+    # delete unneeded addons
+    export FOX_DELETE_AROMAFM=1
     export FOX_DELETE_INITD_ADDON=1
 
-	# version meta
-	export FOX_BUILD_TYPE=Stable
+    # version meta
+    export FOX_BUILD_TYPE=Stable
     export FOX_VERSION=Official
 
     # prevent settings reset
-	export FOX_RESET_SETTINGS=0
+    export FOX_RESET_SETTINGS=0
 
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
+    # extras
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+
+    # let's see what are our build VARs
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+       export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+       export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+       export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+       export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
 else
-	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-	fi
+    if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+        echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+    fi
 fi
