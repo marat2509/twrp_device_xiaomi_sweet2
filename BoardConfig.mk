cda06e752080e529ba8e49c98d2c0ecc2e69fd1a
marat2509 <marat2509@users.noreply.github.com>
2024-01-14 22:54:32 +0300

    Revert "prebuilts: Change kernel (Kewl) and dtbo"
    This reverts commit 0f62ef5eb6d1fe03b737eb8f80ea582c80ced766.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ QCOM_BOARD_PLATFORMS += sm6150
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
-BOARD_KERNEL_IMAGE_NAME := kernel
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE          := 0x00000000
